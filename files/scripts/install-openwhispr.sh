7f38f70030aa167d19d89e6fd70ea06808b258e5
Matt Campbell <matt@battleshopper.com>
2026-02-26 10:05:56 -0600

    Update OpenWhispr installation script to use correct RPM URL format
    - Changed the installation URL in `install-openwhispr.sh` to use the x86_64 architecture for OpenWhispr version 1.5.4, ensuring compatibility with the target system.

--- files/scripts/install-openwhispr.sh
+++ files/scripts/install-openwhispr.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-rpm-ostree install https://github.com/OpenWhispr/openwhispr/releases/download/v1.5.4/OpenWhispr-1.5.4-linux-amd64.rpm
+rpm-ostree install https://github.com/OpenWhispr/openwhispr/releases/download/v1.5.4/OpenWhispr-1.5.4-linux-x86_64.rpm
