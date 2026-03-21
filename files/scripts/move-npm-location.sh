072dc2ffc26a16c2305616ec3d9231fd75760161
Matt Campbell <matt@battleshopper.com>
2026-03-21 11:02:57 -0500

    move npm global location out of the box.

---
+++ files/scripts/move-npm-location.sh
@@ -0,0 +1,11 @@
+# Create a directory for global packages
+mkdir ~/.npm-global
+
+# Configure npm to use it
+npm config set prefix '~/.npm-global'
+
+# Add to your PATH (add this to ~/.bashrc or ~/.zshrc)
+export PATH=~/.npm-global/bin:$PATH
+
+# Reload your shell or source the file
+source ~/.bashrc  # or ~/.zshrc
