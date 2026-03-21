eca47743bd7e9af40418dad8f58ffc4c4c054c2f
Matt Campbell <matt@battleshopper.com>
2026-03-21 11:24:04 -0500

    that didnt work

--- files/scripts/move-npm-location.sh
+++
@@ -1,8 +0,0 @@
-# Create a directory for global packages
-mkdir ~/.npm-global
-
-# Configure npm to use it
-npm config set prefix '~/.npm-global'
-
-# Add to your PATH (add this to ~/.bashrc or ~/.zshrc)
-export PATH=~/.npm-global/bin:$PATH
